47d37f09eb2c79d197fb9fd5538f3295849f2d2c
Kenji Ono <keno@riken.jp>
2014-11-09 09:47:51 +0900

    r26 package 3.0.0

---
+++ include/pmVersion.h
@@ -0,0 +1,25 @@
+#ifndef _PMLIB_VERSION_H_
+#define _PMLIB_VERSION_H_
+
+/* ############################################################################
+ *
+ * PMlib - Performance Monitor library
+ *
+ * Copyright (c) 2010-2011 VCAD System Research Program, RIKEN.
+ * All rights reserved.
+ *
+ * Copyright (c) 2012-2014 Advanced Institute for Computational Science, RIKEN.
+ * All rights reserved.
+ *
+ * ############################################################################
+ */
+
+/**
+ * @file   pmVersion.h
+ * @brief  PMlib version header
+ */
+
+#define PM_VERSION_NO  "3.0.0"
+#define PM_REVISION    "20141106_2000"
+
+#endif /* _PMLIB_VERSION_H_ */
