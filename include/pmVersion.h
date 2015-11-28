b2ffc06dd544cbb5876e9c4c1f4a21f68886f29a
kenoogl <kenoogl@gmail.com>
2015-11-28 14:46:01 +0900

    r42 --with-comp becomes essential

--- include/pmVersion.h
+++ include/pmVersion.h
@@ -19,7 +19,7 @@
  * @brief  PMlib version header
  */
 
-#define PM_VERSION_NO  "4.2.1"
-#define PM_REVISION    "20151124_2300"
+#define PM_VERSION_NO  "4.2.2"
+#define PM_REVISION    "20151128_1300"
 
 #endif /* _PMLIB_VERSION_H_ */
